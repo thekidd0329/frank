775842aed755daa3e46eca94813618cc06c7d541
thekidd0329 <BARTLETTKIDD3@GMAIL.COM>
2026-08-21 19:41:39 -0700

    test: run neural-time regression suite

--- test.sh
+++ test.sh
@@ -18,6 +18,7 @@ java -cp build/frank-tests.jar:build/frank-bones.jar frank.cognition.NewbornStat
 java -cp build/frank-tests.jar:build/frank-bones.jar frank.cognition.NewbornLearningLoopTests
 java -cp build/frank-tests.jar:build/frank-bones.jar frank.cognition.HomeostaticTensionTests
 java -cp build/frank-tests.jar:build/frank-bones.jar frank.cognition.NewbornDynamicsTests
+java -cp build/frank-tests.jar:build/frank-bones.jar frank.cognition.NeuralTimeTests
 java -cp build/frank-tests.jar:build/frank-bones.jar frank.teacher.TeacherSessionTests
 
 if [[ -d prototype/python ]]; then
